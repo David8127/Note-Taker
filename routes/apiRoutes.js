b508afc72d0c5d04b19563a5df9b2dff638009ae
DMartin <davidmartin8127@gmail.com>
2023-01-08 23:07:54 -0800

    functioning post and get routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,52 @@
 const router = require('express').Router();
-const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
-const uuid = require('./helpers/uuid');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
-router.get('/', (req,res)=> {
-    //get all notes from db
-    res.json(`GOT YOUR ${req.method} REQUEST`)
+router.get('/', (req, res) => {
+  //get all notes from db
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-    readFromFile('./db/db.json').then((data) => 
-        {res.json(JSON.parse(data))}
-    );
-})
+router.post('/', (req, res) => {
+  //add a new note to the db
+  const { title, text } = req.body;
 
-router.post('/', (req,res) => {
-    //add a note to the db
-    res.json(`GOT YOUR ${req.method} REQUEST`)
-
-    const { title, text } = req.body;
-
-    if (title && text) {
-      const newNote = {
-        text,
-        title,
-        note_id: uuid(),
-      };
-
+  if (title && text) {
+    const newNote = {
+      text,
+      title,
+      note_id: uuid(),
+    };
 
     readAndAppend(newNote, './db/db.json');
 
     const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      res.json(response);
-    } else {
-      res.json('Error in posting your new note');
-    }
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting your new note');
+  }
+
 });
 
-//BONUS - BE ABLE TO DELETE A NOTE AS WELL
-//review line 140 in index.js
+//Delete route for specific note
+router.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Create a new array of all notes except the one with the ID provided in the URL
+      const updateNote = json.filter((note) => note.note_id !== noteId);
+      // Save that array in db
+      writeToFile('./db/db.json', updateNote);
+
+      // Respond to the DELETE request
+     res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 
 
 module.exports = router;
