3407819a000f40cef43a47378486136f4a81f031
DMartin <davidmartin8127@gmail.com>
2023-01-08 20:29:07 -0800

    added helper function

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
 router.get('/', (req,res)=> {
     //get all notes from db
     res.json(`GOT YOUR ${req.method} REQUEST`)
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
 router.post('/', (req,res) => {
     //add a note to the db
     res.json(`GOT YOUR ${req.method} REQUEST`)
+
+    readAndAppend(newNote, '../db/db.json');
 })
 
 //BONUS - BE ABLE TO DELETE A NOTE AS WELL
